0741bb5a055645f5b29b593a1a074bbf61d1da19
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-08-25 17:11:41 +0530

    Auth0 updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain= {import.meta.env.VITE_AUTH_DOMAIN}
      clientId= {import.meta.env.VITE_AUTH_CLIENT_ID}
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://estate-finder-server.vercel.app/"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
